9a863e5e7c7808d0ff022d7063f26406839fbccb
nihal238 <nihalthumar1999@gmail.com>
2026-06-09 17:05:52 +0530

    update

--- Desktop/portfolio/portfolio/backend/src/db/db.js
+++ Desktop/portfolio/portfolio/backend/src/db/db.js
@@ -1,7 +1,7 @@
 const monegoose = require('mongoose'); 
 async function connectDB() {
   
-        await monegoose.connect("mongodb+srv://admin:PdDdEhefQVBSsAdn@admin.yj6i24x.mongodb.net/database_company")  
+        await monegoose.connect(process.env.mongodb_url)  
       console.log("connected to database"); 
     } 
 module.exports = connectDB;
